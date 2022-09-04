66a7ca89e9c92c21ff9b26d164f351e8c3e24bd2
Esteban Munch-Jones <esteban@Esteban-mbp001.local>
2022-09-04 16:05:02 +0100

    add comments of error

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -13,6 +13,22 @@ const ProductItem = props => {
 
   const toggleFavHandler = () => {
     // toggleFav(props.id);
+    // the problem is that when dispatch is called, triggers the
+    // re-render of this component first, and then of the parent.
+    // That's why I get this error 
+    // Warning: Cannot update a component (`ProductItem`) while rendering a different component (`ProductItem`). To locate the bad setState() call inside `ProductItem`, follow the stack trace as described in https://fb.me/setstate-in-render
+    // in ProductItem (at Products.js:15)
+    // in ul (at Products.js:13)
+    // in Products (created by Context.Consumer)
+    // in Route (at App.js:13)
+    // in main (at App.js:12)
+    // in App (at src/index.js:14)
+    // in Router (created by BrowserRouter)
+    // in BrowserRouter (at src/index.js:13)
+
+    // the difference with using state is that dispatch function wasn't triggering a re-render of this component first
+    // because we were calling the setStates as they were stored in the list (from parents to children).
+
     dispatch({ type: 'TOGGLE_FAV', payload: props.id });
   };
 
