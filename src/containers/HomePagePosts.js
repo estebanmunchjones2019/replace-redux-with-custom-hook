865273200850d80151bb0a05a1681f280b350cd5
Esteban Munch-Jones <esteban@Esteban-mbp001.local>
2022-09-08 19:23:26 +0100

    clean up

--- src/containers/HomePagePosts.js
+++ src/containers/HomePagePosts.js
@@ -1,28 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import React from 'react';
 import { useStore } from "../hooks-store/store";
 
 const Posts = () => {
-    // const [apiData, setApiData] = useState([]);
-
-    // useEffect(() => {
-    //     const fetchData = async() =>{
-    //       const rawData = await fetch("https://jsonplaceholder.typicode.com/posts");
-    //       const data = await rawData.json();
-    //       setApiData(data);
-    //     }
-    //     fetchData();
-    // // see if I get an error with empty dependancy array.
-    //   // and if it works wihtout it.
-    // }, []);
 
     const globalState = useStore()[0];
 
-    // useEffect(() => {
-    //     dispatch('FETCH_POSTS');
-    // },[])
-
-  // then apiData is used in the template
   const postsJSX = globalState.posts.slice(0,9).map(item => {   
     return (
             <li>
@@ -45,24 +28,8 @@ const Posts = () => {
 };
 
 const Widget = () => {
-    // const [apiData, setApiData] = useState([]);
-
-    // useEffect(() => {
-    //     const fetchData = async() =>{
-    //       const rawData = await fetch("https://jsonplaceholder.typicode.com/posts");
-    //       const data = await rawData.json();
-    //       setApiData(data);
-    //     }
-    //     fetchData();
-    // // see if I get an error with empty dependancy array.
-    //   // and if it works wihtout it.
-    // }, []);
 
     const globalState = useStore()[0];
-
-    // useEffect(() => {
-    //     dispatch('FETCH_POSTS');
-    // },[])
 
   // then apiData is used in the template to show only 3 posts
   const postsJSX = globalState.posts.slice(0,2).map(item => {   
@@ -73,7 +40,6 @@ const Widget = () => {
     )
 });
   return (
-// React.fragments and title
       <>
         <h2>Widget component</h2>
         <ul>
